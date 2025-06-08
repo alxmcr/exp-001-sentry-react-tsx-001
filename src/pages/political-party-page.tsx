2479f31d7f35e8d56540de8b5386504a65bcb31c
Alejandro Coca <alxmcr@outlook.com>
2025-06-08 19:23:12 -0400

    wip

--- src/pages/political-party-page.tsx
+++ src/pages/political-party-page.tsx
@@ -1,10 +1,12 @@
+import { Toaster } from "react-hot-toast";
 import { FormPoliticalPartyNew } from "../components/forms/form-political-party-new";
 
 export default function PoliticalPartyPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">Political Party</h1>
-      <FormPoliticalPartyNew  />
+      <FormPoliticalPartyNew />
+      <Toaster />
     </main>
   );
 }
