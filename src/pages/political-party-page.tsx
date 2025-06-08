11df285ad61c847e6f28f86e4bf1747f2dc3f5fa
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-06-08 19:10:18 -0400

    feat/customer-form (#4)

--- src/pages/political-party-page.tsx
+++ src/pages/political-party-page.tsx
@@ -1,7 +1,10 @@
+import { FormPoliticalPartyNew } from "../components/forms/form-political-party-new";
+
 export default function PoliticalPartyPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">Political Party</h1>
+      <FormPoliticalPartyNew  />
     </main>
   );
 }
