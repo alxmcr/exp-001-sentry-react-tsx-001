0adeaf4354268e59e0e8ee36446a42b620b36782
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-06-08 19:06:20 -0400

    chore/react-query-setup (#3)

---
+++ src/hooks/useMutationCreatePoliticalParty.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { PoliticalPartiesAPIImpl } from "../api/political-parties-api.impl";
+import type { PoliticalPartyDataNew } from "../types/service-types";
+
+
+export function useMutationCreatePoliticalParty() {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (politicalParty: PoliticalPartyDataNew) => {
+      const response = await new PoliticalPartiesAPIImpl().create(
+        politicalParty
+      );
+
+      return response;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["politicalParties"] });
+      toast.success("Political party created successfully");
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error("Failed to create political party");
+    },
+  });
+
+  return mutation;
+}
