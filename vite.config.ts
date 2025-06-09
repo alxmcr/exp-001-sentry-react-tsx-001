5ce8b1abb02f314bfcd32739efb2b3a30607b8f3
Alejandro Coca <alxmcr@outlook.com>
2025-06-09 02:33:20 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,13 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig, loadEnv, type PluginOption } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [
-      react(),
-      tailwindcss(),
-      sentryVitePlugin({
-        org: "nextjs-hackathon-global-2025",
-        project: "exp-001-react-tsx",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      }),
-    ] as PluginOption[],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
+    plugins: [react(), tailwindcss()] as PluginOption[],
     server: {
       host: true,
       port: parseInt(env.VITE_APP_PORT) || 3000,
