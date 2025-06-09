25abcc595df22a477697f03e084611e860d8cdc9
Alejandro Coca <alxmcr@outlook.com>
2025-06-08 23:38:32 -0400

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -27,10 +27,26 @@ Sentry.init({
   tracePropagationTargets: ["localhost"],
 });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container, {
+    // Callback called when an error is thrown and not caught by an ErrorBoundary.
+    onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+      console.warn("Uncaught error", error, errorInfo.componentStack);
+    }),
+    // Callback called when React catches an error in an ErrorBoundary.
+    onCaughtError: Sentry.reactErrorHandler(),
+    // Callback called when React automatically recovers from errors.
+    onRecoverableError: Sentry.reactErrorHandler(),
+  });
+  root.render(
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </StrictMode>
+    </Sentry.ErrorBoundary>
+  );
+}
